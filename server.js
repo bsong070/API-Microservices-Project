32d1a2d3cd98c0be3bedb2c2847433a2f4259a62
Brian Song <bsong070@gmail.com>
2021-01-03 23:00:21 -0500

    FCC Timestamp Project

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -21,12 +22,44 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log({greeting:'hello API'}) // outputs in git
   res.json({greeting: 'hello API'});
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+let responseObject = {}
+
+app.get('/api/timestamp/:date', (request,response)=>{
+  let date = request.params.date
+
+  if (date.includes('-')){
+    //Date String
+    responseObject['unix'] = new Date(date).getTime()
+    responseObject['utc'] = new Date(date).toUTCString()
+  }else{
+    //Timestamp
+    date = parseInt(date)
+    
+    responseObject['unix'] = new Date(date).getTime()
+    responseObject['utc'] = new Date(date).toUTCString()
+
+  }
+
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: 'Invalid Date'})
+  }
+  response.json(responseObject)
+})
+
+app.get('/api/timestamp', (request,response)=>{
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+
+  response.json(responseObject)
+})
